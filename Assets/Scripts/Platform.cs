f76ee4565752a132e80e9ca0e3c46203ffb76aac
C14399846 <C14399846@mydit.ie>
2016-03-06 18:12:11 +0000

    replaced collider in platforms with code
    used code in Platform function to do collisions

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BoxCollider2D platformCollider = gameObject.AddComponent<BoxCollider2D>();
+        platformCollider.size = new Vector2(1, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,21 +9,20 @@ public class Player : MonoBehaviour {
     public float speed = 50f;
     public float maxSpeed = 300f;
     public float jumpForce = 300f;
-    public bool grounded;
+    public bool grounded = false;
+    
 
     private Rigidbody2D rb2d;
 
 	// Use this for initialization
-	void Start () {
+	void Start () { 
 
         //player collisions
         Rigidbody2D playerRBody = gameObject.AddComponent<Rigidbody2D>();
          playerRBody.gravityScale = 2f;// strength of gravity
-         playerRBody.angularDrag = 0; // rotation of player
+         playerRBody.angularDrag = 12f; // rotation of player
          playerRBody.drag = 1f;// friction between air, water, ground, etc
          playerRBody.mass = 1.1f; // player mass
-
-
 
 
         rb2d = gameObject.GetComponent<Rigidbody2D>();
@@ -33,11 +32,13 @@ public class Player : MonoBehaviour {
 
 
         BoxCollider2D playerCollider = gameObject.AddComponent<BoxCollider2D>();
-        playerCollider.size = new Vector2(1, 1);
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+          playerCollider.size = new Vector2(1, 1);
+
+        
+}
+
+    // Update is called once per frame
+    void FixedUpdate () {
         
 
         float h = Input.GetAxis("Horizontal");
@@ -55,10 +56,18 @@ public class Player : MonoBehaviour {
         }
 
         //jumping
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") )
         {
             rb2d.AddForce(Vector2.up * jumpForce);
+            //grounded = false;
         }
+
+        //checks if player is air-born
+        /*if(playerCollider.IsTouching)
+        {
+            grounded = true;
+        }*/
+
 
     }
 }
