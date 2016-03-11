f0ba5cc62f89e2c0e98a971e1eb634fa56b17e96
Fanner487 <eamontang1@hotmail.com>
2016-03-11 19:02:59 +0000

    added powerup with script. Destroys itself in collision

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+    private Player player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+       
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
