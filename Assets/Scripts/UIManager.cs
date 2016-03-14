8c28d11bb0677f3f81076d95bb0acc1404dbdff6
storm1996 <alannahsmullins@gmail.com>
2016-03-14 12:39:52 +0000

    Basic Menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public void GoPlay()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void GoOptions()
+	{
+		Application.LoadLevel(2);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
