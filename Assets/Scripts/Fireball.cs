8b6bcac66667c77da77569293524c2c3463e052b
Fanner487 <eamontang1@hotmail.com>
2016-04-20 02:05:36 +0100

    Some code clean up

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -4,57 +4,66 @@ HOLDS FIREBALL MOVEMENT AND COLLISION CODE
 using UnityEngine;
 using System.Collections;
 
-public class Fireball : MonoBehaviour {
+public class Fireball : MonoBehaviour
+{
 
-    /*
-
-        x amount of things a second
-    */
     public Rigidbody2D rb2d;
     private BoxCollider2D boxCollider;
     private float distanceTravelled;
-    private Vector2 lastPosition;
+    private Vector2 lastPosition; // gets distance travelled by fireball
+    private float maxDistance;
     
-
     private float speed = 1000f;
 
     //creates fireball and its collider
-	void Start () {
+    void Start()
+    {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         rb2d.gravityScale = 0;
         boxCollider = gameObject.GetComponent<BoxCollider2D>();
+
+        //gets pos of fireball at time of instantiation
         lastPosition = transform.position;
+        maxDistance = 500f;
     }
 
     //checks if fireball is touching any enemies, destroys them if they are, and explodes
-    void Update(){
+    void Update()
+    {
 
+        //updates total distance travelled by sprite
         distanceTravelled += Vector2.Distance(transform.position, lastPosition);
 
-        if(distanceTravelled > 500f)
+        //despawns if distance exceeded
+        if (distanceTravelled > maxDistance)
         {
             Destroy(gameObject);
         }
-        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Enemy")){
-            if (boxCollider.IsTouching(obj.GetComponent<BoxCollider2D>())){
+
+        //damages/kills enemy on collision
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            if (boxCollider.IsTouching(obj.GetComponent<BoxCollider2D>()))
+            {
                 Player.explode();
                 Destroy(gameObject);
                 obj.GetComponent<Enemy>().Damage(50);
             }
         }
     }
-
     
     //fires
-    public void Fire(string direction){
-        if (direction.Equals("Left")){
+    public void Fire(string direction)
+    {
+        if (direction.Equals("Left"))
+        {
             rb2d.AddForce(Vector2.left * speed);
             rb2d.transform.localScale = new Vector2(-1, 1);
         }
-        else if (direction.Equals("Right")){ rb2d.AddForce(Vector2.right * speed);}
+        else if (direction.Equals("Right")) { rb2d.AddForce(Vector2.right * speed); }
     }
 
     // if fireball hits a platform or the border, it is destroyed
-    public void OnTriggerEnter2D(Collider2D col){ if (col.CompareTag("Border") || col.CompareTag("Ground")){ Destroy(gameObject);}}
+    public void OnTriggerEnter2D(Collider2D col) { if (col.CompareTag("Border") || col.CompareTag("Ground")) { Destroy(gameObject); } }
 
-}//end class
+}
