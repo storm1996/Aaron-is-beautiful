66978761a27d94d54e04d9e4eca099742400898b
storm1996 <alannahsmullins@gmail.com>
2016-04-20 10:46:48 +0100

    Minor Changes

---
+++ Assets/Scripts/DrawGUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI()
+	{
+		GUIStyle style = new GUIStyle ();
+		style.font = (Font)Resources.Load ("Demolition Crack Black", typeof(Font));
+		style.fontSize = 25;
+		GUI.Label (new Rect (10, 80, 100, 20), "DINO DEFENCE", style);
+	}
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,8 +3,19 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
-	public void GoPlay(){ Application.LoadLevel(1);}// play menu
-	public void GoOptions(){ Application.LoadLevel(2);}// options menu
-	public void ExitGame(){ Application.Quit();}// exits game
+	public void GoPlay()
+	{ 
+		Application.LoadLevel(1);
+	}
 
-}//end class
+	public void GoOptions()
+	{ 
+		Application.LoadLevel(2);
+	}
+
+	public void ExitGame()
+	{ 
+		Application.Quit();
+	}
+
+}
